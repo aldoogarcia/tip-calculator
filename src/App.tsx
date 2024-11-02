f4ce634da74bbc19a38084fdd94e198773afa3a9
aldo garcia <aldoredcreo@gmail.com>
2024-11-02 14:45:30 -0600

    Start Project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {menuItems} from './data/bd'
+
+function App() {
+console.log(menuItems)
+
+  return (
+    <>
+      <header>
+        <h1>Tip Calculater</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
